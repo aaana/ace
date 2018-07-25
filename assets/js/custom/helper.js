f034763489e9a96989265369e89aa3e88cd6045c
Anna <593880978@qq.com>
2018-07-25 15:46:24 +0800

    类别管理和token

--- assets/js/custom/helper.js
+++ assets/js/custom/helper.js
@@ -14,11 +14,14 @@ var req = GetRequest();
 var globalToken = req['token'];
 function tokenInit()
 {
-
-var allA=$("a");
-for(var a in allA){
-    var element=$(allA[a]);
-    console.log(element);
-    element.attr("href",element.attr("href")+"?token="+globalToken);
-}
+    $.each($("a"),function (key,value) {
+        console.log(value);
+        $(value).attr("href",$(value).attr("href")+"?token="+globalToken);
+    });
+    // var allA=$("a");
+    // for(var a in allA){
+    //     var element=$(allA[a]);
+    //     console.log(element);
+    //     element.attr("href",element.attr("href")+"?token="+globalToken);
+    // }
 }
